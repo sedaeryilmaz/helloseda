91932c81613842b3b53d1eed01792f4bd070e046
sedae <seda.eryilmaz@ozu.edu.tr>
2017-07-13 04:11:50 -0400

    kot

--- hello.py
+++ hello.py
@@ -1,2 +1,2 @@
 #hello deneme 
-print "Merhaba yine ben! Bugun elbise giydim, birazcik premses oldum. Gerci ben her halimle premsesim. Cok guzelim ya of! Neyse-> Nazar degdirmeyin plz.."
+print "Bugün elbise giymedim :(:(:(:(   Neyse ben her halimle güzelim.."
